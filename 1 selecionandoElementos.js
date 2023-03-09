aa32c69f29c3a6e7f516941caabd27b48b05b397
anammonteiro <anammcosta3@gmail.com>
2023-03-08 21:17:13 -0300

    JavaScript Web

---
+++ 1 selecionandoElementos.js
@@ -0,0 +1,33 @@
+// const element = document.getElementById("main-text")
+
+// console.log(element)
+
+// const element = document.getElementsByClassName("paragraph")
+
+// console.log(element)
+
+// const element = document.getElementsByTagName("p")
+
+// console.log(element)
+
+// const element = document.querySelector("p")
+
+// console.log(element)
+
+/* const element = document.querySelectorAll("p")
+
+element.forEach(element => {
+    console.log(element)
+})
+
+console.log(element)
+
+*/
+
+// const element = document.getElementsByName('my-input')
+
+// console.log(element)
+
+const element = document.querySelector("input")
+
+console.log(element.attributes) 
